e9700d21d7cd16330a89972bb1194d1cdee35307
Nadav <d_lahish@walla.com>
2016-11-18 12:50:39 +0200

    add header

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,18 +5,18 @@ export default class Header extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>iKeyboard</Text>
+        <Text style={styles.text}>iKeyboard</Text>
 
         <View style={styles.input}>
-          <Text>Number of Keys: </Text>
-          <Button
-            onPress={() => this.props.onKeyCountChange(1)}
-            title='+'
-          />
-          <Text>{this.props.keyCount}</Text>
+          <Text style={styles.text}>Number of Keys: </Text>
           <Button
             onPress={() => this.props.onKeyCountChange(-1)}
             title='-'
+          />
+          <Text style={styles.text}>{this.props.keyCount}</Text>
+          <Button
+            onPress={() => this.props.onKeyCountChange(1)}
+            title='+'
           />
         </View>
       </View>
@@ -27,8 +27,17 @@ export default class Header extends Component {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    alignItems: 'center',
     justifyContent: 'space-between',
-    height: 30,
+    alignItems: 'center',
+    height: 35,
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  text: {
+    fontSize: 20
+  },
+  input: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
 })
